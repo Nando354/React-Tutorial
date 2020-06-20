98a97c727ef36507ce5db2209ab4e22fa4bf0c99
mine <fernandoaguirre@mine-2.local>
2020-06-19 22:44:04 -0700

    Tutorial notes clean up

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ const App = props => {
   );
 }
 
-
+export default App;
 //-------------------------Other
 // function App() {
 //   return (
@@ -62,7 +62,7 @@ const App = props => {
 //   );
 // }
 
-export default App;
+
 
 
 
